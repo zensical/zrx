da481eafb36e0453126d1fc523646a73a2cd1192
squidfunk <martin.donath@squidfunk.com>
2026-04-10 08:25:18 +0100

    feature: add `ExactSizeIterator` impls for `Ordered` iterators
    Signed-off-by: squidfunk <martin.donath@squidfunk.com>

--- crates/zrx-store/src/store/decorator/ordered/into_iter.rs
+++ crates/zrx-store/src/store/decorator/ordered/into_iter.rs
@@ -104,3 +104,15 @@ where
         self.ordering.size_hint()
     }
 }
+
+impl<K, V, C> ExactSizeIterator for IntoIter<K, V, C>
+where
+    K: Key,
+    V: Clone,
+{
+    /// Returns the exact remaining length of the iterator.
+    #[inline]
+    fn len(&self) -> usize {
+        self.ordering.len()
+    }
+}
