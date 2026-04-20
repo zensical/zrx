2e88a1755d5a30b272f5848ee343ba1d332b8e7b
squidfunk <martin.donath@squidfunk.com>
2026-04-20 18:54:05 +0200

    feature: add `IntoError` conversion trait and impls for step `Error`
    Signed-off-by: squidfunk <martin.donath@squidfunk.com>

---
+++ crates/zrx-scheduler/src/scheduler/step/error/convert.rs
@@ -0,0 +1,86 @@
+// Copyright (c) 2025-2026 Zensical and contributors
+
+// SPDX-License-Identifier: MIT
+// Third-party contributions licensed under DCO
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+// ----------------------------------------------------------------------------
+
+//! Step error conversions.
+
+use std::error;
+
+use super::Error;
+
+// ----------------------------------------------------------------------------
+// Traits
+// ----------------------------------------------------------------------------
+
+/// Conversion into [`Error`].
+pub trait IntoError {
+    /// Converts into a step error.
+    ///
+    /// This method is intended to be used within [`Action::execute`][], to make
+    /// it convenient to convert an arbitrary error into a step error.
+    ///
+    /// [`Action::execute`]: crate::scheduler::action::Action::execute
+    fn into_error(self) -> Error;
+}
+
+// ----------------------------------------------------------------------------
+// Blanket implementations
+// ----------------------------------------------------------------------------
+
+impl<E> IntoError for E
+where
+    E: error::Error + Send + 'static,
+{
+    /// Converts any error into a step error.
+    ///
+    /// This implementation attempts to downcast the error into one of the known
+    /// variants, and falls back to the [`Error::Other`] variant in other cases.
+    /// It also ensures that the error is not double-boxed, returning unknown
+    /// errors captured by the [`Error::Other`] variant unchanged.
+    #[inline]
+    fn into_error(self) -> Error {
+        let mut err: Box<dyn error::Error + Send> = Box::new(self);
+        err = match err.downcast() {
+            Ok(err) => return *err,
+            Err(err) => err,
+        };
+
+        // Implements downcast attempt of error
+        macro_rules! downcast {
+            ($variant:path) => {
+                err = match err.downcast() {
+                    Ok(err) => return $variant(*err),
+                    Err(err) => err,
+                }
+            };
+        }
+
+        // Downcast known error variants
+        downcast!(Error::Session);
+        downcast!(Error::Scope);
+
+        // Handle unknown error variants
+        Error::Other(err)
+    }
+}
